290273d2269ae11879ce64f6265cf35d5a6b6f95
Shaikh Mahek <mahekshaikh1298@gmail.com>
2025-08-20 13:26:33 +0530

    Initial commit

--- src/Patterns/HollowRectangle.java
+++ src/Patterns/HollowRectangle.java
@@ -7,7 +7,7 @@ public class HollowRectangle {
 
         for (int i = 1; i<=n;i++){
             for (int j = 1; j<=m;j++){
-                //Cell -> (i,j)
+                //Cell ->  (i,j)
                 if (i ==1 || j == 1||i==n||j==m){
                     System.out.print("*");
                 } else {
