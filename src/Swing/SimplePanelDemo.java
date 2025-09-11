5320671abe64d34f43e554c479f9ab2e46b466dd
Shaikh Mahek <mahekshaikh1298@gmail.com>
2025-09-11 14:41:22 +0530

    Add simple JPanel demo inside JFrame

---
+++ src/Swing/SimplePanelDemo.java
@@ -0,0 +1,15 @@
+package Swing;
+
+import javax.swing.*;
+
+public class SimplePanelDemo {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Simple JPanle");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,300);
+        JPanel panel = new JPanel();
+        frame.add(panel);
+
+        frame.setVisible(true);
+    }
+}
