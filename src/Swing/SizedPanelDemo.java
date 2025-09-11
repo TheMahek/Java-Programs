8dd39ce654be2aef79d5f19018656ddcacd65946
Shaikh Mahek <mahekshaikh1298@gmail.com>
2025-09-11 14:42:24 +0530

    Add JPanel demo with custom size and position

---
+++ src/Swing/SizedPanelDemo.java
@@ -0,0 +1,18 @@
+package Swing;
+
+import javax.swing.*;
+import java.awt.*;
+public class SizedPanelDemo {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Sized JPanel");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,300);
+        frame.setLayout(null); //Needed for manual size
+
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.pink);
+        panel.setBounds(50,40,200,100);
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+}
