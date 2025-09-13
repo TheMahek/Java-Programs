ed88de8fe870a0486b2851eeec6e0e2a17c5c00a
Shaikh Mahek <mahekshaikh1298@gmail.com>
2025-09-13 16:42:50 +0530

    Add simple JTextField demo with FlowLayout

---
+++ src/Swing/TextFieldExample.java
@@ -0,0 +1,17 @@
+package Swing;
+
+import javax.swing.*;
+import java.awt.*;
+public class TextFieldExample {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("JTextField Demo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,150);
+        frame.setLayout(new FlowLayout());
+
+        JTextField textField = new JTextField(20); // INPUT Field
+        frame.add(textField);
+
+        frame.setVisible(true);
+    }
+}
